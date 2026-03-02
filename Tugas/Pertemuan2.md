538a6ba36add86c249b52567f4bdd3df6d17717a
galang48 <galangyudha1945@gmail.com>
2026-03-02 09:15:25 +0700

    feat: add authentication and profile management features
    - Implemented user registration, login, and logout functionality.
    - Added email verification process for new users.
    - Created password reset and confirmation features.
    - Developed profile management with options to update information and delete accounts.
    - Introduced modal components for user confirmation actions.
    - Added various UI components including buttons, input fields, and navigation links.
    - Established a responsive layout for the application.
    - Created tests for authentication, email verification, password management, and profile features.

---
+++ Tugas/Pertemuan2.md
@@ -0,0 +1,13 @@
+## Screeshoot Tugas
+
+Tampilan `/about`.
+
+![About](SS/about.png)
+
+Tampilan register
+
+![Register](SS/register.png)
+
+Tampilan login
+
+![Login](SS/login.png)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,8 +52,8 @@
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
-                    <h1 class="mb-1 font-medium">Galang Yudha</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">20230140176</p>
+                    <h1 class="mb-1 font-medium">Nama: Galang Yudha</h1>
+                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Nim: 20230140176</p>
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
                             <a href="https://github.com/code-worker-me/PWF-Genap-2025/blob/main/pertemuan-1.md" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
